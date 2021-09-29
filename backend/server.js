eeb1b108fcb717eee4346f039478a73925c3edbf
Hernan Berisso <hberisso1@gmail.com>
2021-09-29 16:18:50 -0400

    registered, integrated, and connected to MongoDB cluster

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express"
+import connectDB from "./config/db.js"
 import products from "./data/products.js"
 import dotenv from "dotenv"
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
