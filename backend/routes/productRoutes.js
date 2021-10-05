ad7fc3dc01f66787524cc542dc7f047ede77cdd2
Hernan Berisso <hberisso1@gmail.com>
2021-10-05 12:43:15 -0400

    added routes and instance methods for users with hashed password

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,11 @@
 import express from "express"
-import asyncHandler from "express-async-handler"
 const router = express.Router()
-import Product from "../models/productModel.js"
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js"
 
-// GET /api/products *public*
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-  })
-)
-
-// GET /api/prodducts/:id *public*
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error("Product not found")
-    }
-  })
-)
+router.route("/").get(getProducts)
+router.route("/:id").get(getProductById)
 
 export default router
