af64ac17b5516094e6873eaf38db088d401c7c8d
Hernan Berisso <hberisso1@gmail.com>
2021-10-21 14:27:39 -0400

    added capability to edit user info if admin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -115,6 +115,40 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 })
 
+// GET api/users/:id *private/admin* Get user by id
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password")
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
+})
+
+// PUT api/users/:id *private/admin* update user
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin ?? user.isAdmin
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
+})
+
 export {
   authUser,
   getUserProfile,
@@ -122,4 +156,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,6 +13,7 @@ import {
   userRegisterReducer,
   userUpdateProfileReducer,
   userDeleteReducer,
+  userUpdateReducer,
 } from "./reducers/userReducers"
 import {
   orderCreateReducer,
@@ -31,6 +32,7 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   userList: userListReducer,
   userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
